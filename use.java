d2a12de90d7191772cefe8544106f857c7dcece1
David Zambrano <ggmortum@hotmail.com>
2016-10-02 18:04:17 -0500

    Update use.java

--- use.java
+++ use.java
@@ -1,7 +1,7 @@
 import android.view.inputmethod.*;
 
 
-imageButtonBuscar.setOnClickListener(new OnClickListener() {
+btn.setOnClickListener(new OnClickListener() {
 
     public void onClick(View v) {
         InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
